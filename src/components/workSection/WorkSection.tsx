1c049c61e197aeb4981cbae120631a69847446fc
Michael Malleske <mmalleske@gmail.com>
2023-03-08 21:16:58 -0600

    ojo work gallery

--- src/components/workSection/WorkSection.tsx
+++ src/components/workSection/WorkSection.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "./WorkSection.scss";
-import { Card, Modal } from 'antd';
+import { Card, Modal, Carousel } from 'antd';
 
 interface WorkSectionProps {
 }
@@ -49,7 +49,7 @@ const WorkSection: React.FC<WorkSectionProps> = () => {
                     style={{ width: 240 }}
                     cover={<img alt="example" src={require("../../assets/images/ojo.png")} />}
                 >
-                    <Card.Meta title="Ojo Labs" description={<a target="_blank" href="www.homeowner.ojo.com">Visit</a>} />
+                    <Card.Meta title="Ojo Labs" />
 
                 </Card>
                 <Card
@@ -59,7 +59,7 @@ const WorkSection: React.FC<WorkSectionProps> = () => {
                     style={{ width: 240 }}
                     cover={<img alt="example" src={require("../../assets/images/realsavvy.png")} />}
                 >
-                    <Card.Meta title="Real Savvy" description={<a href="www.realsavvy.com" target="_blank"> Visit</a>} />
+                    <Card.Meta title="Real Savvy" />
                 </Card>
                 <Card
                     onClick={() => showModal(2)}
@@ -68,10 +68,43 @@ const WorkSection: React.FC<WorkSectionProps> = () => {
                     style={{ width: 240 }}
                     cover={<img alt="example" src={require("../../assets/images/chickenwaffle.png")} />}
                 >
-                    <Card.Meta title="Chicken Waffle Studios" description={<a target="_blank" href="www.chickenwaffle.com">Visit</a>} />
+                    <Card.Meta title="Chicken Waffle Studios" />
                 </Card>
-                <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                    <p>{modalContents[modalContentsIndex].title}</p>
+                <Modal title="Ojo Labs" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                    <Carousel className="mm-slider">
+                        <div className="mm-slide">
+                            <div className="mm-slide__header">
+                                <iframe width="455" height="210" src="https://www.youtube.com/embed/7otxRER8J1E" title="Ojo Homeowner Demo" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                            </div>
+                            <div className="mm-slide__description">
+                                <p>At <b>Ojo Labs</b> I was a fullstack developer on our <b>Homeowner</b> product - a tool to help users track their home's value, manage home maintenance and find local service providers. Our primary tech stack was <b>React/Typescript</b> frontend and <b>Php/Laravel</b> backend.</p>
+                            </div>
+                        </div>
+                        <div className="mm-slide">
+                            <div className="mm-slide__header">
+                                <img width="455" alt="ojo-demo" src={require("../../assets/images/digs01-2.png")} />
+                            </div>
+                            <div className="mm-slide__description">
+                                <p>Among the many contributions I made during my time at Ojo was the <b>Announcement Engine</b> - a service that gathered data points based on the current market as well as the individual user to deliver personalized and meaningful notifications to our consumers.</p>
+                            </div>
+                        </div>
+                        <div className="mm-slide">
+                            <div className="mm-slide__header">
+                                <img width="455" alt="ojo-demo" src={require("../../assets/images/digs-maintenance-checklist.png")} />
+                            </div>
+                            <div className="mm-slide__description">
+                                <p>I also helped build our <b>Maintenance Checklist</b> feature, which integrates with <b>Thumbtack</b> to provide realtime maintenance suggestions specific to your home and local provider recommendations.</p>
+                            </div>
+                        </div>
+                        <div className="mm-slide">
+                            <div className="mm-slide__header">
+                                <img width="455" alt="ojo-demo" src={require("../../assets/images/digs-et.jpg")} />
+                            </div>
+                            <div className="mm-slide__description">
+                                <p>Outside of the Homeowner app, I also built internal tools to integrate my team's application into the greater Ojo ecosystem. One of these tools utilized RabbitMQ to listen to user interactions with our agents, or "events", and sent out automated invites to our platform. The event system also allowed us to gather insightful analytics.</p>
+                            </div>
+                        </div>
+                    </Carousel>
                 </Modal>
             </div>
         </section>
