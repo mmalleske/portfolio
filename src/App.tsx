a9b9f55094e88621010c4fc0855d3b90b00a5c33
Michael Malleske <mmalleske@gmail.com>
2023-03-12 20:10:13 -0500

    skills section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Title from "./components/title/Title";
 import OjoSection from './components/workSection/OjoSection';
 import SectionHeader from './components/sectionHeader/SectionHeader';
+import SkillsSection from './components/skillsSection/SkillsSection';
 import LazyLoad from 'react-lazy-load';
 
 
@@ -12,7 +13,8 @@ function App() {
     <div className="App">
       {/* <Nav /> */}
       <Title />      
-      <SectionHeader id="skills" title='Skills' />            
+      <SectionHeader id="skills" title='Skills' />        
+      <SkillsSection />    
       <OjoSection />
     </div>
   );
--- src/components/skillsSection/SkillsSection.tsx
+++ src/components/skillsSection/SkillsSection.tsx
@@ -1,49 +1,105 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
+import Title from "../../components/title/Title";
 import LazyLoad from 'react-lazy-load';
+import StoryLine from '../../components/storyLine/StoryLine';
+import YouTube from 'react-youtube';
+import { Button } from 'antd';
+import { RightCircleOutlined } from '@ant-design/icons';
+import { useWindowWidth } from '@react-hook/window-size';
+import { FaCode, FaWrench } from 'react-icons/fa';
 import "./SkillsSection.scss";
 
-interface SkillsSectionProps {
+interface SkillsItemProps {
+    language: string;
+    proficiency: number;
 }
 
-const SkillsSection: React.FC<SkillsSectionProps> = ({ }) => {
+const SkillsItem: React.FC<SkillsItemProps> = ({ language, proficiency }) => {
+    return (
+        <div className="mm-skills-item">
+            <div className="mm-skills-item__label">
+                <span>{`${language}: `}</span>
+            </div>
+            <div className="mm-skills-item__progress-bar">
+                <div style={{ width: `${proficiency}%` }} className="mm-skills-item__progress-bar__mask" >
+                    <div className="mm-skills-item__progress-bar__mask__fill" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function SkillsSection() {
+
+    const windowWidth = useWindowWidth();
+
+    const languageItems = [
+        {
+            language: "HTML/CSS",
+            proficiency: 95
+        },
+        {
+            language: "JavaScript",
+            proficiency: 95
+        },
+        {
+            language: "React/Typescript",
+            proficiency: 90
+        },
+        {
+            language: "Php/Laravel",
+            proficiency: 70
+        },
+        {
+            language: "Ruby/Rails",
+            proficiency: 50
+        },
+        {
+            language: "C#",
+            proficiency: 60
+        },
+        {
+            language: "Python",
+            proficiency: 40
+        },
+    ];
 
     return (
-        <section className="mm-skills">
-            <div className="mm-skills__title">
-                <h2>Skills</h2>
-                <div className="mm-skills__divider-x" />
-                <div className="mm-skills__divider-y" />
+        <div className="mm-skills-section-v2">
+            <div className="mm-skills-section-v2__languages">
+                <LazyLoad height={150} width={'100%'} threshold={0.5}>
+                    <div className="mm-skills-section-v2__subtitle">
+                        <FaCode />
+                        <h2>Languages</h2>
+                        <div className="underline" />
+                    </div>
+                </LazyLoad>
+                <LazyLoad height={300} width={'100%'} threshold={0.5}>
+                    <div className="mm-skills-section-v2__languages__list">
+                        {languageItems.map((item, index) => (
+                            <SkillsItem key={`language-${index}`} proficiency={item.proficiency} language={item.language} />
+                        ))}
+                    </div>
+                </LazyLoad>
             </div>
-            <div className="mm-skills__languages">
-                <div className="mm-skills__subtitle">
-                    <h3>Code</h3>
+            <LazyLoad height={150} width={'100%'} threshold={0.5}>
+                <div className="mm-skills-section-v2__subtitle">
+                    <FaWrench />
+                    <h2>Tools</h2>
+                    <div className="underline" />
                 </div>
-                <ul>
-                    <li>React/Redux/RTKQuery</li>
-                    <li>JavaScript/Typescript</li>
-                    <li>Php/Laravel</li>
-                    <li>Ruby/Rails</li>
-                    <li>Node.js/Ember.js</li>
-                    <li>Python</li>
-                    <li>C#</li>
-                </ul>
-            </div>
-            <div className="mm-skills__tools">
-                <div className="mm-skills__subtitle">
-                    <h3>Tools</h3>
+            </LazyLoad>
+            <LazyLoad height={windowWidth > 968 ? '300px' : '480px'} width={'100%'} threshold={0.5}>
+                <div className="mm-work-section-v2__technologies">
+                    <img alt="tech-icon" src={require("../../assets/images/rabbitmqlogo.png")} />
+                    <img alt="tech-icon" src={require("../../assets/images/dockerlogo.webp")} />
+                    <img alt="tech-icon" src={require("../../assets/images/kubelogo.png")} />
+                    <img alt="tech-icon" src={require("../../assets/images/grpc-icon-color.png")} />
+                    <img alt="tech-icon" src={require("../../assets/images/AirflowLogo.png")} />
                 </div>
-                <ul>
-                    <li>SQL/MongoDB</li>
-                    <li>Docker</li>
-                    <li>Unity 3D/Unreal Engine</li>
-                    <li>Photoshop</li>
-                    <li>AWS</li>
-                    <li>Apache Airflow</li>
-                    <li>gRPC</li>
-                </ul>
-            </div>
-        </section>
+            </LazyLoad>
+        </div>
     );
-};
+}
 
 export default SkillsSection;
